cc7338ce89ac1e26ae946b2ca74f7ed614c8a943
Charles Perkins <chuck@kuracali.com>
2019-06-01 11:39:54 -0700

    add launch and watch scripts

---
+++ launch.sh
@@ -0,0 +1,1 @@
+/usr/bin/qemu-system-x86_64 -machine accel=kvm -name guest=uefiguest -machine pc,accel=kvm,usb=off,dump-guest-core=off -smp 2,sockets=2,cores=1,threads=1 -uuid 515645b7-ab3a-4e82-ba62-25751e4b523f -bios ./OVMF.fd -m 1G -vga qxl -spice port=5900,addr=127.0.0.1,disable-ticketing -drive file=uefi.iso
